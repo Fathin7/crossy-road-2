8188370b51d012cf55a1428149474c64f6a32cf2
Fathin7 <82428254+Fathin7@users.noreply.github.com>
2021-06-09 17:06:40 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,24 @@
+class Player{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.spt = createSprite(this.x , this.y , 50 , 50);
+    this.spt.shapeColour = "orange";
+    
+
+
+
+   }
+
+ move(xdir , ydir){
+     this.spt.x += xdir*grid;
+     this.spt.y += ydir*grid;
+ }
+
+
+
+
+
+
+
+} 
